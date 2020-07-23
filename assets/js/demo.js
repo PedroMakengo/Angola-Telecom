d0d8000e18ba6d65c8b4925ed90c6bcf9c2bb5f1
PedroMakengo <pedromakengomiguel@gmail.com>
2020-07-23 16:05:56 +0100

    Trabalho de hoje

---
+++ assets/js/demo.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    let avanca = $("#avanca");
+    let recua = $("#recua");
+
+    let primeiro_segmento = $("#primeiro_segmento");
+    let segundo_segmento = $("#segundo_segmento");
+
+    // segundo_segmento.fadeOut();
+
+    // Avançar para outro segmento
+    // avanca.click(function() {
+    //     primeiro_segmento.slideUp("slow", function() {
+    //         avanca.fadeOut("slow", function() {
+    //             recua.fadeIn("slow");
+    //         });
+    //     });
+    //     segundo_segmento.fadeIn("slow")
+    // });
+
+    // Recuar no segmento
+    // recua.click(function() {
+    //     primeiro_segmento.fadeOut();
+    //     segundo_segmento.slideUp("slow", function() {
+    //         recua.fadeOut("slow", function() {
+    //             avanca.fadeIn("slow");
+    //         });
+    //     });
+    //     primeiro_segmento.fadeIn("slow");
+    // });
+});
